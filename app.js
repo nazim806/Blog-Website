05bca977056be9a6e598b07e0bba327f566db34f
Nazim Uddin <umnazim@gmail.com>
2020-04-15 19:13:07 -0700

    new page for individual post

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -21,11 +22,10 @@ app.use(express.static("public"));
 let posts = [];
 
 app.get("/", function (req, res) {
-  res.render("home", { 
-    startingContent: homeStartingContent, 
-    posts:posts 
+  res.render("home", {
+    startingContent: homeStartingContent,
+    posts: posts,
   });
-  
 });
 
 app.get("/about", function (req, res) {
@@ -48,6 +48,20 @@ app.post("/compose", function (req, res) {
   posts.push(post);
 
   res.redirect("/");
+});
+
+app.get("/posts/:postTitle", function (req, res) {
+  const requestedTitle = _.lowerCase(req.params.postTitle);
+
+  posts.forEach(function (post) {
+    const storedTitle = _.lowerCase(post.title);
+    if (storedTitle === requestedTitle) {
+      res.render("post", {
+        title: post.title,
+        content: post.content,
+      });
+    }
+  });
 });
 
 app.listen(3000, function () {
